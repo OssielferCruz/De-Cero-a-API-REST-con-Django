42e1b0de7dcaa7ad2ac183a4900e0fba45b73c51
Fernando Ossiel Cruz Medina <ossielfer.cruz@gmail.com>
2026-03-17 11:01:06 -0600

    feat: exponer CRUD de pacientes con DRF y rutas API

---
+++ api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Paciente
+
+
+class PacienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Paciente
+        fields = '__all__'
+        read_only_fields = ('fecha_registro',)
